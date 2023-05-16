32770d841633d00c400ad36f3b4250bc94fe328c
AtsukiUsui <kmmm13037@gmail.com>
2023-05-16 14:39:02 +0900

    Update/sudo

--- src/illuminometer_pub.cpp
+++ src/illuminometer_pub.cpp
@@ -1,15 +1,16 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
-#include <cstdlib>
-#include <cstdio>
-#include <iostream>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ros::init(argc, argv, "td_usb_publisher");
   ros::NodeHandle nh;
-  ros::Publisher pub = nh.advertise<std_msgs::Float64>("luminous_intensity", 1000);
+  ros::Publisher pub =
+      nh.advertise<std_msgs::Float64>("luminous_intensity", 1000);
 
-  FILE* pipe = popen("sudo ./td-usb iws660 get read --loop=500", "r");
+  FILE *pipe = popen("./td-usb iws660 get read --loop=500", "r");
   if (!pipe) {
     ROS_ERROR("Failed to execute command");
     return 1;
